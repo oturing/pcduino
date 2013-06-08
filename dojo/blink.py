4ea22cc98ba2dd1f0f142ae81b0bd9273cb90c89
Luciano Ramalho <luciano@ramalho.org>
2013-06-08 10:37:09 +0000

    avancando no blink.py

--- dojo/blink.py
+++ dojo/blink.py
@@ -11,8 +11,8 @@ piscar o ponto decimal no display de 7 segmentos do circuito 'Dojo com pcDuino'
 
 import time, os, sys
 
-GPIO_MODE_PATH= os.path.normpath('/sys/devices/virtual/misc/gpio/mode/')
-GPIO_PIN_PATH=os.path.normpath('/sys/devices/virtual/misc/gpio/pin/')
+GPIO_MODE_PATH = '/sys/devices/virtual/misc/gpio/mode/'
+GPIO_PIN_PATH = '/sys/devices/virtual/misc/gpio/pin/'
 
 HIGH = "1"
 LOW =  "0"
@@ -20,18 +20,18 @@ INPUT = "0"
 OUTPUT = "1"
 
 def setup():
-    with open(pin, 'r+') as f: ## open the file in r/w mode
+    with open(GPIO_MODE_PATH+pin, 'r+') as f: ## open the file in r/w mode
         f.write(OUTPUT)        ## set the mode of the pin
 
-    with open(pin, 'r+') as f: ## open the file in r/w mode
+    with open(GPIO_PIN_PATH+pin, 'r+') as f: ## open the file in r/w mode
         f.write(LOW)
 
 def set(pin, value):
-    with open(pinData[pin], 'r+') as pin_file:
+    with open(GPIO_PIN_PATH+pin, 'r+') as pin_file:
         pin_file.write(str(value))
 
-setup()
 pin = sys.argv[1]
+setup()
 while True:
     set(pin, HIGH)
     sleep(1)
