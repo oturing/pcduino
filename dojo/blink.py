90b5e521f5bcca560887c483a9313ecef0baf6d4
Luciano Ramalho <luciano@ramalho.org>
2013-06-08 16:47:33 +0000

    analog e blink preparados para o dojo do Tosconf 2013

--- dojo/blink.py
+++ dojo/blink.py
@@ -7,32 +7,37 @@ piscar o ponto decimal no display de 7 segmentos do circuito 'Dojo com pcDuino'
 
 # fonte:
 # https://learn.sparkfun.com/tutorials/programming-the-pcduino/accessing-gpio-pins
-# https://learn.sparkfun.com/tutorials/programming-the-pcduino/analog-input-and-output
 
-import time, os, sys
+import time
 
-GPIO_MODE_PATH = '/sys/devices/virtual/misc/gpio/mode/'
-GPIO_PIN_PATH = '/sys/devices/virtual/misc/gpio/pin/'
+GPIO_PATH = '/sys/devices/virtual/misc/gpio/'
+GPIO_MODE_PATH = GPIO_PATH + 'mode/'
+GPIO_DATA_PATH = GPIO_PATH + 'pin/'
 
-HIGH = "1"
-LOW =  "0"
-INPUT = "0"
-OUTPUT = "1"
+OUTPUT_MODE = "1"
+PONTO_DECIMAL = 3
+ON = "1"
+OFF = "0"
+
+DEBUG = True
 
 def setup():
-    with open(GPIO_MODE_PATH+pin, 'r+') as f: ## open the file in r/w mode
-        f.write(OUTPUT)        ## set the mode of the pin
-
-    with open(GPIO_PIN_PATH+pin, 'r+') as f: ## open the file in r/w mode
-        f.write(LOW)
+    for i in range(8):
+        with open(GPIO_MODE_PATH+'gpio%s'%i, 'w') as f:
+            f.write(OUTPUT_MODE)
+        with open(GPIO_DATA_PATH+'gpio%s'%i, 'w') as f:
+            f.write(OFF)
 
 def set(pin, value):
-    with open(GPIO_PIN_PATH+pin, 'r+') as pin_file:
-        pin_file.write(str(value))
+    with open(GPIO_DATA_PATH+'gpio%s'%pin, 'w') as f:
+        f.write(str(value))
 
-pin = sys.argv[1]
+pin = PONTO_DECIMAL
 setup()
 while True:
-    set(pin, HIGH)
-    sleep(1)
-    set(pin, LOW)
+    set(pin, ON)
+    print pin, 'ON'
+    time.sleep(1)
+    set(pin, OFF)
+    print pin, 'OFF'
+    time.sleep(1)
